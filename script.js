dd024489272ac3725fd18725dc8b1eeca44c696a
Christopher Foose <chrisfoose@gmail.com>
2022-03-23 20:02:46 -0700

    Connecting API

---
+++ script.js
@@ -0,0 +1,9 @@
+/*const hatchways(url) = {
+    method: 'GET',
+    url : 'https://api.hatchways.io/assessment/students'
+}; */
+
+const response = await fetch('url', {
+    method: 'GET',
+    url : 'https://api.hatchways.io/assessment/students';
+)};
