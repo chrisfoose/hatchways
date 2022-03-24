c7d5567d39b6e3f76f19f0e50c4ffb2394933908
Christopher Foose <chrisfoose@gmail.com>
2022-03-24 14:43:52 -0700

    api stuff

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
     method: 'GET',
     url : 'https://api.hatchways.io/assessment/students'
 }; */
-
+url = 'https://api.hatchways.io/assessment/students';
+try {
 async function getData() {
     const response = await fetch('url', {
         method: 'GET',
         url : 'https://api.hatchways.io/assessment/students'
     })
+    if (response.ok) {
+        const jsonResponse = await response.json();
+            renderResponse(jsonResponse);}
+    }
+} catch(error) {
+    console.log(error);
 };
+
